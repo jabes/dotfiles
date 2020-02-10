6c7bc4d155805eafd2b022e0f14035e3c48ef2de
Justin Bull <bull.justin@gmail.com>
2020-02-10 16:46:57 -0500

    Check if file exists before grep-ing it

--- bin/scripts/search-file-contents.sh
+++ bin/scripts/search-file-contents.sh
@@ -2,7 +2,11 @@
 
 # https://stackoverflow.com/a/16957078
 function search-file-contents() {
-    local FILE_PATH=$1
-    local SEARCH_PATTERN=$2
-    grep -rnw "$FILE_PATH" -e "$SEARCH_PATTERN"
+	local FILE=$1
+	local SEARCH=$2
+	if [[ -f "$FILE" ]]; then
+		grep -rnw "$FILE" -e "$SEARCH"
+	else
+		echo "File not found: '$FILE'"
+	fi
 }
